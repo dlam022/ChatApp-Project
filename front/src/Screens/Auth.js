d3a8286289d7ac5d360f08ab9e6640a45b478a73
DiscoDoggy <akaattarilim@gmail.com>
2023-06-03 00:40:29 -0700

    Lab 6 setup, Please work

---
+++ front/src/Screens/Auth.js
@@ -0,0 +1,93 @@
+import react from "react";
+import Form from "../Components/form.js";
+import { Button } from "@mui/material";
+
+class Auth extends react.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            showForm: false,
+            selectedForm: undefined,
+        }
+    }
+
+    closeForm = () => {
+        this.setState({showForm: false});
+    }
+
+    login = (data) => {
+        // TODO: write codes to login
+        //connect to server, pass information that user has entered to server
+            //perform actions depending on what user enteres
+        console.log(data);
+
+        fetch(this.props.server_url + '/api/auth/login', {
+            method: "POST",
+            mode: 'cors',
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                'Accept': "application/json"
+            },
+
+            body:JSON.stringify(data),
+
+        }).then((res) => {
+            res.json().then((data) => {
+                if (data.msg == "Logged in"){
+                    // this.setState({screen: "lobby"});
+                    this.props.changeScreen("lobby");
+                }
+                else{
+                    alert(data.msg);
+                    //this.setState({screen: "auth"});
+                }
+            });
+        });
+
+        
+    }
+
+    register = (data) => {
+        // TODO: write codes to register
+        console.log(data);
+    }
+
+    render(){
+        let display = null;
+        if (this.state.showForm){
+            let fields = [];
+            if (this.state.selectedForm === "login"){
+                fields = ['username', 'password'];
+                display = <Form fields={fields} 
+                close={this.closeForm} 
+                type="login" 
+                submit={this.login} 
+                key={this.state.selectedForm}/>;
+            }
+            else if (this.state.selectedForm === "register"){
+                fields = [ 'username', 'password', 'name'];
+                display = <Form
+                    fields={fields} 
+                    close={this.closeForm} 
+                    type="register" 
+                    submit={this.register} 
+                    key={this.state.selectedForm}/>;
+            }   
+        }
+        else{
+            display = <div>
+                <Button onClick={() => this.setState({showForm: true, selectedForm:"login"})}> Login </Button>
+                <Button onClick={() => this.setState({showForm: true, selectedForm: "register"})}> Register </Button>
+                </div>              ;
+        }
+        return(
+            <div>
+                <h1> Welcome to our website! </h1>
+                {display}
+            </div>
+        );
+    }
+}
+
+export default Auth;
---
+++ front/src/Screens/Lobby.js
@@ -0,0 +1,43 @@
+import react from "react";
+import { Button } from "@mui/material";
+
+class Lobby extends react.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            rooms: undefined,
+        }
+    }
+
+    componentDidMount(){
+        // TODO: write codes to fetch all rooms from server
+        fetch(this.props.server_url + '/api/rooms/all', {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then((res) => {
+            res.json().then((data) => {
+                this.setState({rooms:data});
+            })
+        });
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h1>Lobby</h1>
+                {this.state.rooms ? this.state.rooms.map((room) => {
+                    return <Button variant="contained" key={"roomKey"+room} onClick={() => alert(room)}>{room}</Button>
+                }) : "loading..."}
+                {/* write codes to join a new room using room id*/}
+                {/* write codes to enable user to create a new room*/}
+                
+            </div>
+        );
+    }
+}
+
+export default Lobby;
